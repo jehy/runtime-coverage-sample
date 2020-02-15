bf4a657fbc08112c51d4b01461cf35f5d35d44db
Jehy <fate@jehy.ru>
2020-02-15 23:50:05 +0300

    add simple sample

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const runtimeCoverage = require('runtime-coverage');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+app.get('/startCoverage', async (req, res) => {
+  await runtimeCoverage.startCoverage();
+  res.send('coverage started');
+});
+
+
+app.get('/getCoverage', async (req, res) => {
+
+  const options = {
+    all: req.query.all,
+    return: true,
+    reporters: [req.query.reporter || 'text'],
+  };
+  const coverage = await runtimeCoverage.getCoverage(options);
+  const data = Object.values(coverage)[0];
+  res.end(data);
+});
+
+app.get('/getCoverageWithStream', async (req, res) => {
+
+  const options = {
+    all: req.query.all,
+    return: true,
+    stream: true,
+    reporters: [req.query.reporter || 'text'],
+  };
+  const coverage = await runtimeCoverage.getCoverage(options);
+  const stream = Object.values(coverage)[0];
+  stream.pipe(res);
+});
+
+app.listen(3000);
